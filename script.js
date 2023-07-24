9ecb7a3579302ce2ddfc911e450057c2a2a6f4be
Jonathan Domingo <jonodomingo@gmail.com>
2023-07-23 21:17:28 -0700

    Variable rename. Removal of switch case in favor of array based audio element access.
    Signed-off-by: Jonathan Domingo <jonodomingo@gmail.com>

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
 function play() {
     console.log("March 7th clicked!");
 
-    var audio;
-    var element = document.getElementById("march");
-    var randomMarchNum = Math.floor(Math.random() * 3);
-
     // Play a random March 7th voice clip.
-    switch(randomMarchNum) {
-        case 0:
-            audio = document.getElementById("audio0");
-            break;
-        case 1:
-            audio = document.getElementById("audio1");
-            break;
-        case 2:
-            audio = document.getElementById("audio2");
-            break;
-    }
-    audio.play();
+    var m7AudioArray = new Array(
+        document.getElementById("VO_March_7th_Basic_ATK_01"), 
+        document.getElementById("VO_March_7th_Technique_01"),
+        document.getElementById("VO_March_7th_Turn_Begins_02")
+    );
+    var randomMarchNum = Math.floor(Math.random() * m7AudioArray.length);
+    m7AudioArray[randomMarchNum].play();
 
     // Animate button press.
-    element.classList.add("animateDescriptor");
-    element.addEventListener( "animationend",  function() {
-      element.classList.remove("animateDescriptor");    
+    var m7Img = document.getElementById("march");
+    m7Img.classList.add("animateDescriptor");
+    m7Img.addEventListener( "animationend",  function() {
+        m7Img.classList.remove("animateDescriptor");    
     } );
 }
