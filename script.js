b063b2557c2ba3c51d73a256c85e4792319f333b
Jonathan Domingo <jonodomingo@gmail.com>
2023-07-23 20:01:03 -0700

    Implemented animation and random audio playing on click.

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-
-
 function play() {
-    var audio = document.getElementById("audio");
+    console.log("March 7th clicked!");
+
+    var audio;
+    var element = document.getElementById("march");
+    var randomMarchNum = Math.floor(Math.random() * 3);
+
+    switch(randomMarchNum) {
+        case 0:
+            audio = document.getElementById("audio0");
+            break;
+        case 1:
+            audio = document.getElementById("audio1");
+            break;
+        case 2:
+            audio = document.getElementById("audio2");
+            break;
+    }
+
     audio.play();
-    console.log("played audio");
+
+    element.classList.add("animateDescriptor");
+    element.addEventListener( "animationend",  function() {
+      element.classList.remove("animateDescriptor");    
+    } );
 }
-
-var animateButton = function(e) {
-
-    e.preventDefault;
-    //reset animation
-    e.target.classList.remove('animate');
-    
-    e.target.classList.add('animate');
-    setTimeout(function(){
-      e.target.classList.remove('animate');
-    },700);
-  };
-  
-  var bubblyButtons = document.getElementsByClassName("bubbly-button");
-  
-  for (var i = 0; i < bubblyButtons.length; i++) {
-    bubblyButtons[i].addEventListener('click', animateButton, false);
-  }
-  
